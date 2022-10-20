5b4d847cbeefcaaa8052b68242b0002bd5af377b
CTM <clemente1875gmail.com>
2022-10-20 21:58:44 +0200

    inicio calculadora

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola que tal</Text>
+    </View>
+  );
+};
+
+export default App;
